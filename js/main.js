1a8e3c325b93fcc507f0138177091b403ad279ff
neretin-trike <hawktrike@gmail.com>
2019-04-11 18:51:07 +0700

    update(js): добавление плавности движения

--- js/main.js
+++ js/main.js
@@ -1,40 +1,49 @@
-window.requestAnimFrame = (function(){ 
-    return  window.requestAnimationFrame       || 
-            window.webkitRequestAnimationFrame || 
-            window.mozRequestAnimationFrame    || 
-            window.oRequestAnimationFrame      || 
-            window.msRequestAnimationFrame     || 
-            function( callback ){ 
-              window.setTimeout(callback, 1000 / 60); 
-            }; 
-  })();
-
 let playingField = document.getElementById("playing_field");
-
 let ctx = playingField.getContext("2d");
 
-drawTriangle();
+let x = 0;
+let y = 0;
+let velX = 0;
+let velY = 0;
+let maxSpeed = 20;
+let friction = 0.98;
+let keyPress = 0;
 
 document.onkeydown = function (e) {
-    if (e.keyCode == 39) {
-        drawTriangle(5);
-    } 
-    if (e.keyCode == 37) {
-        drawTriangle(-5);
-    }
+    keyPress = e.keyCode;
+}
+document.onkeyup = function (e) {
+    keyPress = 0;
 }
 
-function drawTriangle(x) {
+function Update() {
+    requestAnimationFrame(Update);
+
+    if (keyPress == 39) {
+        if (velX < maxSpeed) {
+            velX++;
+        }
+    }
+    if (keyPress == 37) {
+        if (velX > -maxSpeed) {
+            velX--;
+        }
+    }
+
+    velX *= friction;
+    x += velX;
+
     ctx.clearRect(0,0,playingField.width,playingField.height); 
     ctx.fillStyle = "red";
-    ctx.beginPath();
-    ctx.moveTo(200,50);
-    ctx.lineTo(200,100);
-    ctx.lineTo(250,75);
-    ctx.closePath();
     ctx.strokeStyle = "darkred";
     ctx.lineWidth = 5;
+    ctx.beginPath();
+    ctx.moveTo(200+x,50);
+    ctx.lineTo(200+x,100);
+    ctx.lineTo(250+x,75);
+    ctx.closePath();
     ctx.fill();
     ctx.stroke();
-    ctx.translate(x,0);
 }
+
+Update();
