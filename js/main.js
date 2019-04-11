c93a0fc6c974ccc0984b8b28d606c89f69bf23de
neretin-trike <hawktrike@gmail.com>
2019-04-11 20:51:35 +0700

    update(js): добавление направления движения по вертикали

--- js/main.js
+++ js/main.js
@@ -5,42 +5,57 @@ let x = 0;
 let y = 0;
 let velX = 0;
 let velY = 0;
-let maxSpeed = 20;
-let friction = 0.98;
-let keyPress = 0;
+let maxSpeed = 10;
+let coefSpeed = 4;
+let friction = 0.78;
+let keyPress = {};
 
 document.onkeydown = function (e) {
-    keyPress = e.keyCode;
+    keyPress[e.keyCode] = true;
 }
 document.onkeyup = function (e) {
-    keyPress = 0;
+    keyPress[e.keyCode] = false;
 }
 
 function Update() {
     requestAnimationFrame(Update);
 
-    if (keyPress == 39) {
+    if (keyPress[39]) {
         if (velX < maxSpeed) {
-            velX++;
+            velX+=coefSpeed;
         }
     }
-    if (keyPress == 37) {
+    if (keyPress[37]) {
         if (velX > -maxSpeed) {
-            velX--;
+            velX-=coefSpeed;
+        }
+    }
+
+    if (keyPress[40]) {
+        if (velY < maxSpeed) {
+            velY+=coefSpeed;
+        }
+    }
+    if (keyPress[38]) {
+        if (velY > -maxSpeed) {
+            velY-=coefSpeed;
         }
     }
 
     velX *= friction;
     x += velX;
 
+    velY *= friction;
+    y += velY;
+
     ctx.clearRect(0,0,playingField.width,playingField.height); 
     ctx.fillStyle = "red";
     ctx.strokeStyle = "darkred";
     ctx.lineWidth = 5;
     ctx.beginPath();
-    ctx.moveTo(200+x,50);
-    ctx.lineTo(200+x,100);
-    ctx.lineTo(250+x,75);
+    ctx.moveTo(200+x,50+y);
+    ctx.lineTo(200+x,100+y);
+    ctx.lineTo(250+x,75+y);
     ctx.closePath();
     ctx.fill();
     ctx.stroke();
