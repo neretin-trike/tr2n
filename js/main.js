5d2f8f260636d18ae0ac2c1b34109de26987e4d0
neretin-trike <hawktrike@gmail.com>
2019-04-12 23:05:15 +0700

    update(js): добавление трасировки при движении объекта

--- js/main.js
+++ js/main.js
@@ -11,6 +11,7 @@ let friction = 0.78;
 let keyPress = {};
 let angle = 0;
 let rad = 0;
+let traceArr = [];
 
 document.onkeydown = function (e) {
     keyPress[e.keyCode] = true;
@@ -31,6 +32,8 @@ function Update() {
     if (keyPress[38] == true) {
     	velX += Math.cos(rad)*5;
         velY += Math.sin(rad)*5;
+
+        traceArr.push( {x: velX, y: velY} );
     }
     if (keyPress[40] == true) {
     	velX -= Math.cos(rad)*5;
@@ -40,6 +43,21 @@ function Update() {
     ctx.clearRect(0,0,playingField.width,playingField.height);
     
     ctx.beginPath();
+    ctx.strokeStyle="red";
+    for (let i=0; i<traceArr.length; i++) {
+      ctx.arc(traceArr[i].x,traceArr[i].y,2.5,0,Math.PI*2);
+    }
+    ctx.shadowColor = "red";
+    ctx.shadowOffsetX = 0;
+    ctx.shadowOffsetY = 0; 
+    ctx.shadowBlur = 10;
+    ctx.lineWidth = 2;
+    ctx.stroke();
+    ctx.closePath();
+
+    ctx.lineWidth = 1;
+    ctx.shadowBlur = 0;  
+    ctx.beginPath();
     ctx.strokeStyle="green";
     ctx.arc(velX,velY,10,0,Math.PI*2);
     ctx.stroke();
@@ -48,6 +66,7 @@ function Update() {
     ctx.save();
     ctx.translate(velX,velY);
     ctx.rotate(rad);
+    ctx.strokeStyle="blue";
     ctx.strokeRect(-15/2+20,-10/2,15,10);
     ctx.restore();
     
