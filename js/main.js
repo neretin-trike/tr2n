27c175292e8c4729af6779bb11a861e6c305fdb2
neretin-trike <hawktrike@gmail.com>
2019-04-12 22:59:01 +0700

    update(js): добавление возможности выбирать угол поворота и двигаться в этом направлении

--- js/main.js
+++ js/main.js
@@ -3,13 +3,14 @@ let ctx = playingField.getContext("2d");
 
 let x = 0;
 let y = 0;
-let velX = 0;
-let velY = 0;
+let velX = 150;
+let velY = 150;
 let maxSpeed = 10;
 let coefSpeed = 4;
 let friction = 0.78;
 let keyPress = {};
 let angle = 0;
+let rad = 0;
 
 document.onkeydown = function (e) {
     keyPress[e.keyCode] = true;
@@ -21,61 +22,36 @@ document.onkeyup = function (e) {
 function Update() {
     requestAnimationFrame(Update);
 
-    if (keyPress[38]) {
-        velX+=10;
-        velY+=10;
-        // if (velX < maxSpeed) {
-        //     velX+=coefSpeed;
-        // }
+    if (keyPress[37] == true) {
+    	angle-=10;
     }
-    if (keyPress[40]) {
-        velX-=10;
-        velY-=10;
-        // if (velX > -maxSpeed) {
-        //     velX-=coefSpeed;
-        // }
+    if (keyPress[39] == true) {
+    	angle+=10;
     }
-
-    if (keyPress[39]) {
-        angle+=3;
-        // if (velY < maxSpeed) {
-        //     velY+=coefSpeed;
-        // }
+    if (keyPress[38] == true) {
+    	velX += Math.cos(rad)*5;
+        velY += Math.sin(rad)*5;
     }
-    if (keyPress[37]) {
-        angle-=3;
-        // if (velY > -maxSpeed) {
-        //     velY-=coefSpeed;
-        // }
+    if (keyPress[40] == true) {
+    	velX -= Math.cos(rad)*5;
+        velY -= Math.sin(rad)*5;
     }
 
-    // velX *= friction;
-    // x += velX;
-
-    // velY *= friction;
-    // y += velY;
-
-
-    let rad = angle*Math.PI/180;
-
-    x = Math.cos(rad)*velX;
-    y = Math.sin(rad)*velY;
+    ctx.clearRect(0,0,playingField.width,playingField.height);
     
-    ctx.clearRect(0,0,playingField.width,playingField.height); 
     ctx.beginPath();
-    ctx.save();
-    ctx.translate(x,y);
-    ctx.rotate(rad);
-    // ctx.fillStyle = "red";
-    // ctx.strokeStyle = "darkred";
-    // ctx.lineWidth = 5;
-    ctx.moveTo(25,0);
-    ctx.lineTo(25,30);
-    ctx.lineTo(75,15);
-    // ctx.closePath();
-    ctx.fill();
+    ctx.strokeStyle="green";
+    ctx.arc(velX,velY,10,0,Math.PI*2);
     ctx.stroke();
+    ctx.closePath();
+  
+    ctx.save();
+    ctx.translate(velX,velY);
+    ctx.rotate(rad);
+    ctx.strokeRect(-15/2+20,-10/2,15,10);
     ctx.restore();
+    
+    rad = angle*Math.PI/180;
 }
 
 Update();
