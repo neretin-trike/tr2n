06acc38940def6d36643b3a7d09921fcd287cdbc
neretin-trike <hawktrike@gmail.com>
2019-04-14 00:52:27 +0700

    refactor(js): добавление создания трассировки в классе

--- js/main.js
+++ js/main.js
@@ -5,17 +5,9 @@ playingField.height = window.innerHeight;
 
 let ctx = playingField.getContext("2d");
 
-//let velX = 150;
-//let velY = 150;
 let keyPress = {};
-let traceArr = [];
 
-
-
-let colX = 0;
-let colY = 0;
-
-let itHit = false;
+let isCollision = false;
 
 function bindKeyControlls(forward, backward, turnLeft, turnRight) {
     return {
@@ -32,9 +24,6 @@ function setPoint(x, y) {
     }
 }
 
-// let lightCycleColor = "#6fc3df";
-let lightCycleColor = "#df740c";
-
 class LightCycle {
     constructor(options) {
         let { startPosition,
@@ -43,7 +32,7 @@ class LightCycle {
              rotationSens, 
              keyControlls, 
              spriteSrc, 
-             traceLength,
+             traceMaxLength,
              contextCanvas} = options;
 
         this.x = startPosition.x;
@@ -52,7 +41,7 @@ class LightCycle {
         this.maxSpeed = maxSpeed;
         this.rotationSens = rotationSens;
         this.keyControlls = keyControlls;
-        this.traceLength = traceLength; 
+        this.traceMaxLength = traceMaxLength; 
         this.contextCanvas = contextCanvas;
 
         this._img = new Image();
@@ -60,8 +49,11 @@ class LightCycle {
         this._angle = 0;
         this._rad = 0;
         this._collision = setPoint(0,0);
+        this._traceArr = [];
     }
     render() {
+        this.drawTrace();
+
         this.contextCanvas.beginPath();
         this.contextCanvas.shadowBlur = 0;
         this.contextCanvas.lineWidth = 1;
@@ -90,6 +82,31 @@ class LightCycle {
 
         this.update();
     }
+    drawTrace() {
+        this.contextCanvas.beginPath();
+        this.contextCanvas.lineWidth = 4;
+        this.contextCanvas.strokeStyle = this.mainColor;
+        this.contextCanvas.shadowColor = this.mainColor;
+        this.contextCanvas.shadowBlur = 10;
+        for (let i=0; i<this._traceArr.length; i++) {
+
+            this.contextCanvas.lineTo(this._traceArr[i].x,this._traceArr[i].y);
+    
+          if (this._traceArr.length > 148) {
+            let dX = this._traceArr[i].x-this._collision.x;
+            let dY = this._traceArr[i].y-this._collision.y;
+            let lenght = Math.sqrt(dX*dX+dY*dY);
+      
+            let sumRadius = 4+10;
+    
+            if (lenght<sumRadius) {
+                console.log("ПРОИЗОШЛО ПЕРЕСЕЧЕНИЕ");
+                isCollision = true;
+            }
+          }
+        }
+        this.contextCanvas.stroke();
+    }
     update() {
         if (keyPress[this.keyControlls.TURN_LEFT] == true) {
             this._angle -= 7;
@@ -101,10 +118,10 @@ class LightCycle {
             this.x += Math.cos(this._rad)*this.maxSpeed;
             this.y += Math.sin(this._rad)*this.maxSpeed;
     
-            // traceArr.push( {x: velX, y: velY} );
-            // if (traceArr.length > 150) {
-            //     traceArr.shift();
-            // }
+            this._traceArr.push( {x: this.x, y: this.y} );
+            if (this._traceArr.length > this.traceMaxLength) {
+                this._traceArr.shift();
+            }
         }
         if (keyPress[this.keyControlls.BACKWARD] == true) {
             this.x -= Math.cos(this._rad)*this.maxSpeed;
@@ -112,7 +129,7 @@ class LightCycle {
         }
 
         this._collision.x = this.x + 50*Math.cos(this._rad);
-        this._collision.y = this.y + 50*Math.cos(this._rad);
+        this._collision.y = this.y + 50*Math.sin(this._rad);
 
         this._rad = this._angle*Math.PI/180;
     }
@@ -125,7 +142,7 @@ let optionsBlueCycle = {
     rotationSens: 7,
     keyControlls: bindKeyControlls(38, 40, 37, 39),
     spriteSrc:"images/light-cycle.png",
-    traceLength: 150,
+    traceMaxLength: 150,
     contextCanvas: ctx
 }
 let blueLightCycle = new LightCycle(optionsBlueCycle);
@@ -137,7 +154,7 @@ let optionsOrangeCycle = {
     rotationSens: 7,
     keyControlls: bindKeyControlls(87, 83, 65, 68),
     spriteSrc:"images/light-cycle.png",
-    traceLength: 150,
+    traceMaxLength: 150,
     contextCanvas: ctx
 }
 let orangeLightCycle = new LightCycle(optionsOrangeCycle);
@@ -152,6 +169,8 @@ document.onkeyup = function (e) {
 
 function Update() {
     ctx.clearRect(0,0,playingField.width,playingField.height);
+
+    myAnim = requestAnimationFrame(Update);
 
     // if (keyPress[37] == true) {
     // 	angle -= 7;
@@ -173,97 +192,13 @@ function Update() {
     //     velY -= Math.sin(rad)*5;
     // }
 
-
-    // ctx.beginPath();
-    // ctx.save();
-    // ctx.translate(-velX*0.5,-velY*0.5);
-    // ctx.fillStyle = "#2a3849";
-    // for (let i = 0; i<80; i++) {
-    //     for (let j = 0; j<40; j++) {
-    //         ctx.moveTo(25+i*50,j*50+25);
-    //         ctx.arc(25+i*50,j*50+25, 1,0,Math.PI*2);
-    //     }
-    // }
-    // ctx.fill();
-    // ctx.closePath();
-    // ctx.restore();
-
-    // ctx.beginPath();
-    // ctx.save();
-    // ctx.translate(-velX,-velY);
-    // ctx.strokeStyle = "#1e2f46";
-    // ctx.lineWidth = 1;
-    // for (let i = 0; i<50; i++) {
-    //     ctx.moveTo(i*100,0);
-    //     ctx.lineTo(i*100,4000);
-
-    //     ctx.moveTo(0,i*100);
-    //     ctx.lineTo(4000, i*100);
-    // }
-    // ctx.stroke();
-    // ctx.closePath();
-    // ctx.restore();
-
-    ctx.beginPath();
-    ctx.lineWidth = 4;
-    ctx.strokeStyle = lightCycleColor;
-    ctx.shadowColor = lightCycleColor;
-    ctx.shadowBlur = 10;
-    for (let i=0; i<traceArr.length; i++) {
-      ctx.lineTo(traceArr[i].x,traceArr[i].y);
-
-      if (traceArr.length > 148) {
-        let AC = traceArr[i].x-colX;
-        let BC = traceArr[i].y-colY;
-        let lenght = Math.sqrt(AC*AC+BC*BC);
-  
-        let sumRadius = 4+10;
-
-        if (lenght<sumRadius) {
-            itHit = true;
-        }
-      }
-    }
-    ctx.stroke();
-
     blueLightCycle.render();
-    orangeLightCycle.render();
+    // orangeLightCycle.render();
 
-
-    // ctx.beginPath();
-    // ctx.shadowBlur = 0;
-    // ctx.lineWidth = 1;
-    // ctx.strokeStyle = "green";
-    // ctx.arc(velX,velY,10,0,Math.PI*2);
-    // ctx.stroke();
-    // ctx.closePath();
-  
-    // ctx.save();
-    // ctx.translate(velX,velY);
-    // ctx.rotate(rad);
-    // ctx.drawImage(img, -20,-20, 170/2, 82/2);
-
-    // colX = velX+50*Math.cos(rad);
-    // colY = velY+50*Math.sin(rad);
-
-    // ctx.fillStyle = lightCycleColor;
-    // ctx.shadowColor = lightCycleColor;
-    // ctx.shadowBlur = 10;
-    // ctx.fillRect(-9,11,12,3);
-    // ctx.fillRect(-9,-13,12,3);
-    // ctx.fillRect(41,11,12,3);
-    // ctx.fillRect(41,-13,12,3);
-    // ctx.restore();
-
-    
-
-    if (itHit) {
-        console.log("ПРОИЗОШЛО ПЕРЕСЕЧЕНИЕ");
+    if (isCollision) {
+        console.log("КАВО НЕ СЛЫШУ");
         cancelAnimationFrame(myAnim);
     }
-    
-
-    myAnim = requestAnimationFrame(Update);
 }
 
 Update();
